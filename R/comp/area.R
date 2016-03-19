d0b18bff45b43e2453163a142b5cd3f483c33130
Sebastian <wahani@gmail.com>
2016-03-19 13:39:26 +0100

    update area level simulation

--- R/comp/area.R
+++ R/comp/area.R
@@ -2,8 +2,8 @@ modules::import(stats)
 modules::import(saeRobust)
 
 maxIter <- 200
-maxIterParam <- 100
-maxIterRe <- 10000
+maxIterParam <- 5
+maxIterRe <- 1000
 
 fh <- function(dir_name, dir_var_name, pred_name = "fh") {
   force(dir_var_name); force(pred_name)
@@ -55,6 +55,76 @@ rsfh <- function(dir_name, dir_var_name, pred_name = "rsfh") {
     )
     dat[[pred_name]] <- fit$reblup
     dat[[paste0(pred_name, "-BC")]] <- predict(fit, type = "reblupbc")$reblupbc
+    dat
+  }
+}
+
+stfh <- function(dir_name, dir_var_name, pred_name = "sfh") {
+  force(dir_var_name); force(pred_name)
+  formula <- as.formula(paste(dir_name, "~", "x"))
+  function(dat) {
+
+    T <- length(unique(dat$idT))
+    D <- length(unique(dat$idD))
+
+    dat$dirVar <- dat[[dir_var_name]]
+    fit <- try({
+      sae::eblupSTFH(formula, D, T, dirVar, proxmat = testRook(D), data = dat,
+                     MAXITER = maxIter * maxIterParam)
+    })
+    dat[[pred_name]] <- if (inherits(fit, "try-error")) NA else as.numeric(fit$eblup)
+    dat
+  }
+}
+
+tfh <- function(dir_name, dir_var_name, pred_name = "rfh") {
+  force(dir_var_name); force(pred_name)
+  formula <- as.formula(paste(dir_name, "~", "x"))
+  function(dat) {
+    T <- length(unique(dat$idT))
+    fit <- saeRobust::rfh(
+      formula, dir_var_name, data = dat,
+      correlation = corAR1(T), k = 1e6,
+      maxIter = maxIter, maxIterParam = maxIterParam, maxIterRe = maxIterRe
+    )
+    dat[[pred_name]] <- fit$reblup
+    dat
+  }
+}
+
+rtfh <- function(dir_name, dir_var_name, pred_name = "rfh") {
+  force(dir_var_name); force(pred_name)
+  formula <- as.formula(paste(dir_name, "~", "x"))
+  function(dat) {
+    T <- length(unique(dat$idT))
+    fit <- saeRobust::rfh(
+      formula, dir_var_name, data = dat,
+      correlation = corAR1(T),
+      maxIter = maxIter, maxIterParam = maxIterParam, maxIterRe = maxIterRe
+    )
+    dat[[pred_name]] <- fit$reblup
+    dat[[paste0(pred_name, "-BC")]] <- predict(fit, type = "reblupbc")$reblupbc
+    dat
+  }
+}
+
+rstfh <- function(dir_name, dir_var_name, pred_name = "rfh") {
+  force(dir_var_name); force(pred_name)
+  formula <- as.formula(paste(dir_name, "~", "x"))
+  function(dat) {
+
+    T <- length(unique(dat$idT))
+    D <- length(unique(dat$idD))
+
+    fit <- saeRobust::rfh(
+      formula, dir_var_name, data = dat,
+      correlation = corSAR1AR1(W = testRook(D), nTime = T),
+      maxIter = maxIter, maxIterParam = maxIterParam, maxIterRe = maxIterRe
+    )
+
+    dat[[pred_name]] <- fit$reblup
+    dat[[paste0(pred_name, "-BC")]] <- predict(fit, type = "reblupbc")$reblupbc
+
     dat
   }
 }
--- R/simAreaLevel.R
+++ R/simAreaLevel.R
@@ -12,20 +12,26 @@ gg <- modules::use("./R/graphics")
 D <- 40
 nCont <- round(0.1 * D)
 n <- 1
+T <- 10
 trueVar <- rev(seq(2, 6, length.out = D))
 sigre <- 2
 
 # Settings
-runs <- 500
+runs <- 100
 cpus <- parallel::detectCores() - 1
 # set.seed(1)
 reRun <- FALSE
+reRunTemporal <- FALSE
+
+# Graphic Parameter
+width <- 7
+height <- 3
+fontSize <- 14
 
 # Defining the Setups
 setup <- base_id(D, n) %>%
   # Area-Level Data
   sim_gen_e(sd = sqrt(trueVar)) %>%
-  sim_gen_v(sd = 1) %>%
   sim_gen_generic(gen$x$fixed_sequence, groupVars = "idD", name = "x") %>%
   sim_resp_eq(y = 100 + 5 * x + v + e) %>%
   sim_comp_agg(comp_var(trueVar = trueVar)) %>%
@@ -58,6 +64,48 @@ setupSpatialOutlier <- setupSpatial %>%
     nCont = nCont) %>%
   sim_simName("(0.5, u)")
 
+# Temporal Scenarios
+
+trueVarTemporal <- rep(trueVar, each = T)
+
+setupTemporal <- base_id_temporal(D, n, T) %>%
+  # Area-Level Data
+  sim_gen_e(sd = sqrt(trueVarTemporal)) %>%
+  sim_gen_generic(gen$x$fixed_sequence, groupVars = "idD", name = "x") %>%
+  sim_resp_eq(y = 100 + 5 * x + v + e) %>%
+  sim_comp_agg(comp_var(trueVar = trueVarTemporal))
+# %>%
+#   sim_comp_agg(comp$area$tfh("y", "trueVar", "TFH")) %>%
+#   sim_comp_agg(comp$area$rtfh("y", "trueVar", "RTFH")) %>%
+#   sim_comp_agg(comp$area$stfh("y", "trueVar", "STFH")) %>%
+#   sim_comp_agg(comp$area$rstfh("y", "trueVar", "RSTFH"))
+
+setupTemporalBase <- setupTemporal %>%
+  sim_gen(gen_v_norm(sd = sigre)) %>%
+  sim_agg(function(dat) { dat$idC <- FALSE; dat }) %>%
+  sim_simName("(0, 0)")
+
+setupSpatioTemporal <- setupTemporal %>%
+  sim_gen(gen_v_sar(rho = 0.5, sd = sqrt(sigre), name = "v")) %>%
+  sim_gen(gen_v_ar1(rho = 0.5, sd = sqrt(sigre), name = "ar")) %>%
+  sim_resp_eq(y = y + ar) %>%
+  sim_agg(function(dat) { dat$idC <- FALSE; dat }) %>%
+  sim_simName("(0.5, 0)")
+
+setupTemporalBaseOutlier <- setupTemporalBase %>%
+  sim_gen_cont(
+    gen_norm(100, 10, "v"),
+    type = "area", areaVar = "idD", fixed = TRUE,
+    nCont = nCont) %>%
+  sim_simName("(0, u)")
+
+setupSpatioTemporalOutlier <- setupSpatioTemporal %>%
+  sim_gen_cont(
+    gen_norm(100, 10, "v"),
+    type = "area", areaVar = "idD", fixed = TRUE,
+    nCont = nCont) %>%
+  sim_simName("(0.5, u)")
+
 # Trigger Simulation:
 simFun <- . %>%
   sim(runs,
@@ -76,19 +124,45 @@ if (reRun) {
   load("R/data/areaLevel.RData")
 }
 
+if (reRunTemporal) {
+
+  simFun <- . %>%
+    sim(runs,
+        mode = "multicore", cpus = cpus, mc.preschedule = FALSE,
+        path = "./R/data/areaLevelTemporal", overwrite = FALSE) %>%
+    do.call(what = rbind)
+
+  lapply(
+    list(setupTemporalBase, setupTemporalBaseOutlier, setupSpatioTemporal,
+         setupSpatioTemporalOutlier),
+    simFun
+  )
+  simDataTemporal <- sim_read_data("./R/data/areaLevelTemporal")
+  save(list = "simDataTemporal", file = "R/data/areaLevelTemporal.RData")
+} else {
+  load("R/data/areaLevelTemporal.RData")
+}
+
 # To check if there are any errors. This only happens rarely so I do not see
 # reason to take action:
 simData %>% mutar(~is.na(SFH), n ~ length(unique(idR)), by = c("simName"))
 simData <- simData %>% mutar(~!is.na(SFH))
+# simData %>% mutar(function(col) any(is.na(col))) # check for missings
+#
+# simDataTemporal %>% mutar(function(col) any(is.na(col))) # check for missings
+# ar is missing in scenarios with no ar process
+
 
 # Graphics:
 simData$popMean <- simData$y - simData$e
 simData$Direct <- simData$y
 
+simData <- as.DataFrame(simData)
+
 ggDat <- reshape2::melt(
   simData,
   id.vars = c("idD", "popMean", "simName"),
-  measure.vars = c("Direct", "FH", "RFH", "SFH", "RSFH"),
+  measure.vars = c("Direct", "FH", "RFH", "SFH", "RSFH", "RFH.BC", "RSFH.BC"),
   variable.name = "method",
   value.name = "prediction"
 )
@@ -98,11 +172,59 @@ ggDat <- ggDat %>%
         RRMSE ~ sqrt(mean(((prediction - popMean) / popMean)^2)),
         by = c("idD", "method", "simName"))
 
-area_level_mc_rrmse_all <- gg$plots$mse(ggDat) + scale_y_log10()
-area_level_mc_rbias_all <- gg$plots$bias(ggDat)
+scenarioOrder <- c("(0, 0)", "(0, u)", "(0.5, 0)", "(0.5, u)")
+methodOrder <- c("Direct", "FH", "SFH", "RFH", "RSFH", "RFH.BC", "RSFH.BC")
+
+ggDat <- ggDat %>%
+  mutar(simName ~ factor(simName, ordered = TRUE, levels = scenarioOrder),
+        method ~ factor(method, ordered = TRUE, levels = methodOrder))
+
+cairo_pdf("figs/area_level_rrmse.pdf", width, 2 * height)
+gg$plots$mse(ggDat, fontsize = fontSize) +
+  scale_y_log10(breaks = c(0.01, 0.02, 0.1))
+dev.off()
+
+cairo_pdf("figs/area_level_rbias.pdf", width, 2 * height)
+gg$plots$bias(ggDat, fontsize = fontSize)
+dev.off()
 
 ggDat %>% as.data.frame %>% mutar(~ RRMSE > 0.03)
 
+## Graphics Temporal
+
+simDataTemporal$popMean <- simDataTemporal$y - simDataTemporal$e
+simDataTemporal$Direct <- simDataTemporal$y
+
+simDataTemporal <- as.DataFrame(simDataTemporal)
+
+ggDat <- reshape2::melt(
+  simDataTemporal,
+  id.vars = c("idD", "popMean", "simName"),
+  measure.vars = c("Direct", "TFH", "RTFH", "STFH", "RSTFH", "RTFH.BC", "RSTFH.BC"),
+  variable.name = "method",
+  value.name = "prediction"
+)
+
+ggDat <- ggDat %>%
+  mutar(RBIAS ~ mean((prediction - popMean) / popMean),
+        RRMSE ~ sqrt(mean(((prediction - popMean) / popMean)^2)),
+        by = c("idD", "method", "simName"))
+
+scenarioOrder <- c("(0, 0)", "(0, u)", "(0.5, 0)", "(0.5, u)")
+methodOrder <- c("Direct", "TFH", "STFH", "RTFH", "RSTFH", "RTFH.BC", "RSTFH.BC")
+
+ggDat <- ggDat %>%
+  mutar(simName ~ factor(simName, ordered = TRUE, levels = scenarioOrder),
+        method ~ factor(method, ordered = TRUE, levels = methodOrder))
+
+cairo_pdf("figs/area_level_rrmse.pdf", width, 2 * height)
+gg$plots$mse(ggDat, fontsize = fontSize) +
+  scale_y_log10(breaks = c(0.01, 0.02, 0.1))
+dev.off()
+
+cairo_pdf("figs/area_level_rbias.pdf", width, 2 * height)
+gg$plots$bias(ggDat, fontsize = fontSize)
+dev.off()
 
 
 
